519170d3c44bc09ee11a4b2b249bdd0b36245cc2
acgnwang <acgnwang@qq.com>
2021-01-21 14:14:06 +0800

    Site updated: 2021-01-21 14:13:36

--- js/sakura-app.js
+++ js/sakura-app.js
@@ -819,8 +819,14 @@ function grin (tag, type, before, after) {
 function add_copyright () {
   document.body.addEventListener('copy', function (e) {
     if (!mashiro_global.is_user_logged_in && window.getSelection().toString().length > 30) {
-      setClipboardText(e)
+      //判断是否允许复制，false不允许，添加复制作者信息
+      if(mashiro_option.copyright_enable.indexOf("false")!= -1 && window.getSelection().toString().length > mashiro_option.copyright_minCharNumber){
+        setClipboardText(e)
+      }else{
+        addComment.createButterbar('复制成功！<br>Copied to clipboard successfully!', 1000)
+      }
     }
+    
   })
 
   function setClipboardText (event) {
